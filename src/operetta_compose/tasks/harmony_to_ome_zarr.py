62d412d8312a8b3a21f76bd671f48c610677e450
fdsteffen <35061965+fdsteffen@users.noreply.github.com>
2025-02-24 13:06:51 +0100

    Skip indexed files if corresponding TIFF is not present

--- src/operetta_compose/tasks/harmony_to_ome_zarr.py
+++ src/operetta_compose/tasks/harmony_to_ome_zarr.py
@@ -66,7 +66,12 @@ def harmony_to_ome_zarr(
     image_list_updates = []
     for img_path in img_paths:
         img_path = Path(img_path)
-        plate = img_path.parent.name + ".zarr"
+        if img_path.parent.name:
+            print(img_path.parent.name)
+            plate = img_path.parent.name + ".zarr"
+        else:
+            logging.info(f"No plate name can be extracted, default to plate.zarr")
+            plate = "plate.zarr"
         zarr_path = Path(zarr_dir).joinpath(plate)
         df_wells, df_imgs = _parse_harmony_index(img_path)
         msg = f"Converting Harmony TIFFs from {img_path.parent.name} to OME-ZARR"
@@ -228,32 +233,15 @@ def _create_ome_zarr(
     cols = df_wells["col"].unique()
     dataset = 0
 
-    write_plate_metadata(
-        root,
-        list(map(lambda row: string.ascii_uppercase[row - 1], rows)),
-        list(map(str, cols)),
-        [
-            f"{string.ascii_uppercase[row-1]}/{col}"
-            for row, col in zip(df_wells["row"], df_wells["col"])
-        ],
-        name=zarr_url.name,
-        field_count=df_imgs["field"].max(),
-        acquisitions=[{"id": 1, "name": "aq1"}],
-    )
-
     unique_wells = df_wells.drop_duplicates()
     pbar = utils._initialize_pbar(len(unique_wells), msg)
     image_list_updates = list()
+    actual_wells = []
     for _, i in unique_wells.iterrows():
         row = i.row
         row_alpha = string.ascii_uppercase[row - 1]
         col = i.col
         well = f"{row_alpha}/{col}"
-        well_group = root.require_group(well)
-        field_group = well_group.require_group(f"{dataset}")
-        table_group = field_group.require_group("tables")
-
-        write_well_metadata(well_group, images=[{"path": str(dataset)}])
 
         min_pos_z = df_imgs["pos_z"].min()
         min_pos_y = df_imgs["pos_y"].min()
@@ -295,27 +283,15 @@ def _create_ome_zarr(
 
         ad_fov = ad.AnnData(
             X=fov_phys.values,
-            dtype=float,
         )
         ad_fov.obs_names = fov_phys.index
         ad_fov.var_names = fov_phys.columns
 
         ad_well = ad.AnnData(
             X=well_phys.values,
-            dtype=float,
         )
         ad_well.obs_names = well_phys.index
         ad_well.var_names = well_phys.columns
-
-        ad_fov.write_zarr(f"{zarr_url}/{well}/{dataset}/tables/FOV_ROI_table")
-        ad_well.write_zarr(f"{zarr_url}/{well}/{dataset}/tables/well_ROI_table")
-        table_group.attrs["tables"] = ["FOV_ROI_table", "well_ROI_table"]
-        io.write_table_metadata(
-            f"{zarr_url}/{well}/{dataset}", "roi_table", "FOV_ROI_table"
-        )
-        io.write_table_metadata(
-            f"{zarr_url}/{well}/{dataset}", "roi_table", "well_ROI_table"
-        )
 
         fovs_pxls = io.convert_ROI_table_to_indices(
             ad_fov,
@@ -336,6 +312,7 @@ def _create_ome_zarr(
             ),
             dtype=np.uint16,
         )
+        empty_fov = []
         for idx_t, timepoint in enumerate(timepoints):
             for idx_c, channel in enumerate(channels):
                 for idx_z, pos_z in enumerate(planes):
@@ -345,104 +322,136 @@ def _create_ome_zarr(
                     for field in df["field"].values:
                         img_name = df.query(f"field == {field}")["img_name"].iloc[0]
                         fov = fovs_pxls.loc[f"{field - 1}"]
-                        img[
-                            idx_t,
-                            idx_c,
-                            idx_z,
-                            fov["s_y"] : fov["e_y"],
-                            fov["s_x"] : fov["e_x"],
-                        ] = imread(img_path.joinpath(img_name).as_posix())[0]
-
-        img = img[0, :, :, :, :]
-        try:
-            if compute:
-                img = np.array(img)
-            write_image(
-                image=img,
-                group=field_group,
-                axes=[
-                    # {"name": "t", "type": "time", "unit": "second"},
-                    {"name": "c", "type": "channel"},
-                    {"name": "z", "type": "space", "unit": "micrometer"},
-                    {"name": "y", "type": "space", "unit": "micrometer"},
-                    {"name": "x", "type": "space", "unit": "micrometer"},
-                ],
-                coordinate_transformations=[
-                    [
-                        {
-                            "scale": [
-                                # 1.0,
-                                1.0,
-                                df_imgs.iloc[0]["res_z"],
-                                df_imgs.iloc[0]["res_y"] * coarsening_xy**level,
-                                df_imgs.iloc[0]["res_x"] * coarsening_xy**level,
-                            ],
-                            "type": "scale",
-                        }
-                    ]
-                    for level in range(5)
-                ],
+                        try:
+                            img[
+                                idx_t,
+                                idx_c,
+                                idx_z,
+                                fov["s_y"] : fov["e_y"],
+                                fov["s_x"] : fov["e_x"],
+                            ] = imread(img_path.joinpath(img_name).as_posix())[0]
+                            empty_fov.append(False)
+                        except ValueError:
+                            pass
+                            empty_fov.append(True)
+                            # logging.info(f"{img_name} not found in image path")
+        if not all(empty_fov):
+            actual_wells.append(well)
+            well_group = root.require_group(well)
+            field_group = well_group.require_group(f"{dataset}")
+            table_group = field_group.require_group("tables")
+            write_well_metadata(well_group, images=[{"path": str(dataset)}])
+            table_group.attrs["tables"] = ["FOV_ROI_table", "well_ROI_table"]
+            ad_fov.write_zarr(f"{zarr_url}/{well}/{dataset}/tables/FOV_ROI_table")
+            ad_well.write_zarr(f"{zarr_url}/{well}/{dataset}/tables/well_ROI_table")
+            io.write_table_metadata(
+                f"{zarr_url}/{well}/{dataset}", "roi_table", "FOV_ROI_table"
             )
-        except ContainsArrayError:
-            raise FileExistsError(
-                f"{zarr_url} already contains an OME-ZARR fileset. To ignore the existing dataset set overwrite = True."
+            io.write_table_metadata(
+                f"{zarr_url}/{well}/{dataset}", "roi_table", "well_ROI_table"
             )
 
-        omero_channels_updated = []
-        for channel in df_imgs["channel"].unique():
-            wavelength_id = df_imgs.query(f"channel == {channel}")[
-                "MainEmissionWavelength"
-            ].iloc[0]["#text"]
-            ome_chan = next(
-                (oc for oc in omero_channels if oc.wavelength_id == wavelength_id),
-                OmeroNgffChannel(wavelength_id=wavelength_id),
-            )
-            label = df_imgs.query(f"channel == {channel}")["ChannelName"].iloc[0]
-            color = COLORS[channel - 1] if channel < 6 else COLORS[-1]
-            active = True
-            max_int = int(
-                df_imgs.query(f"channel == {channel}")["MaxIntensity"].iloc[0]
-            )
-            window = {
-                "start": 0,
-                "end": max_int,
+            img = img[0, :, :, :, :]
+            try:
+                if compute:
+                    img = np.array(img)
+                write_image(
+                    image=img,
+                    group=field_group,
+                    axes=[
+                        # {"name": "t", "type": "time", "unit": "second"},
+                        {"name": "c", "type": "channel"},
+                        {"name": "z", "type": "space", "unit": "micrometer"},
+                        {"name": "y", "type": "space", "unit": "micrometer"},
+                        {"name": "x", "type": "space", "unit": "micrometer"},
+                    ],
+                    coordinate_transformations=[
+                        [
+                            {
+                                "scale": [
+                                    # 1.0,
+                                    1.0,
+                                    df_imgs.iloc[0]["res_z"],
+                                    df_imgs.iloc[0]["res_y"] * coarsening_xy**level,
+                                    df_imgs.iloc[0]["res_x"] * coarsening_xy**level,
+                                ],
+                                "type": "scale",
+                            }
+                        ]
+                        for level in range(5)
+                    ],
+                )
+            except ContainsArrayError:
+                raise FileExistsError(
+                    f"{zarr_url} already contains an OME-ZARR fileset. To ignore the existing dataset set overwrite = True."
+                )
+
+            omero_channels_updated = []
+            for channel in df_imgs["channel"].unique():
+                wavelength_id = df_imgs.query(f"channel == {channel}")[
+                    "MainEmissionWavelength"
+                ].iloc[0]["#text"]
+                ome_chan = next(
+                    (oc for oc in omero_channels if oc.wavelength_id == wavelength_id),
+                    OmeroNgffChannel(wavelength_id=wavelength_id),
+                )
+                label = df_imgs.query(f"channel == {channel}")["ChannelName"].iloc[0]
+                color = COLORS[channel - 1] if channel < 6 else COLORS[-1]
+                active = True
+                max_int = int(
+                    df_imgs.query(f"channel == {channel}")["MaxIntensity"].iloc[0]
+                )
+                window = {
+                    "start": 0,
+                    "end": max_int,
+                }
+
+                if ome_chan.label is None:
+                    ome_chan.label = label
+                if ome_chan.color is None:
+                    ome_chan.color = color
+                if ome_chan.active is None:
+                    ome_chan.active = active
+                if ome_chan.window is None:
+                    ome_chan.window = OmeroNgffWindow(**window)
+                if ome_chan.window.min is None:
+                    ome_chan.window.min = 0
+                if ome_chan.window.max is None:
+                    ome_chan.window.max = max_int
+                omero_channels_updated.append(ome_chan.to_dict())
+
+            field_group.attrs["omero"] = {
+                "channels": omero_channels_updated,
+                "pixel_size": {
+                    "x": df_imgs.iloc[0]["res_x"],
+                    "y": df_imgs.iloc[0]["res_y"],
+                    "z": df_imgs.iloc[0]["res_z"],
+                },
             }
 
-            if ome_chan.label is None:
-                ome_chan.label = label
-            if ome_chan.color is None:
-                ome_chan.color = color
-            if ome_chan.active is None:
-                ome_chan.active = active
-            if ome_chan.window is None:
-                ome_chan.window = OmeroNgffWindow(**window)
-            if ome_chan.window.min is None:
-                ome_chan.window.min = 0
-            if ome_chan.window.max is None:
-                ome_chan.window.max = max_int
-            omero_channels_updated.append(ome_chan.to_dict())
+            attributes = {"plate": str(zarr_url.name), "well": f"{row_alpha}{col}"}
 
-        field_group.attrs["omero"] = {
-            "channels": omero_channels_updated,
-            "pixel_size": {
-                "x": df_imgs.iloc[0]["res_x"],
-                "y": df_imgs.iloc[0]["res_y"],
-                "z": df_imgs.iloc[0]["res_z"],
-            },
-        }
+            is_3D = True if len(planes) > 1 else False
 
-        attributes = {"plate": str(zarr_url.name), "well": f"{row_alpha}{col}"}
-
-        is_3D = True if len(planes) > 1 else False
-
-        image_list_updates.append(
-            {
-                "zarr_url": f"{str(zarr_url)}/{well}/{dataset}",
-                "attributes": attributes,
-                "types": {"is_3D": is_3D},
-            }
-        )
+            image_list_updates.append(
+                {
+                    "zarr_url": f"{str(zarr_url)}/{well}/{dataset}",
+                    "attributes": attributes,
+                    "types": {"is_3D": is_3D},
+                }
+            )
         pbar.update()
+
+    rows, cols = zip(*[well.split("/") for well in actual_wells])
+    write_plate_metadata(
+        group=root,
+        rows=list(set(rows)),
+        columns=list(set(cols)),
+        wells=actual_wells,
+        name=zarr_url.name,
+        field_count=df_imgs["field"].max(),
+        acquisitions=[{"id": 1, "name": "aq1"}],
+    )
 
     return image_list_updates
 
